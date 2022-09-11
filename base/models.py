10a5902aa470281ba994f1b1f9da3a22af469c67
Jiří Trnečka <trnecka.jiri@gmail.com>
2022-09-11 13:54:50 +0200

    Založen kompletní ORM a vytvořena stránka na hledání

--- base/models.py
+++ base/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import Model
+
 
 # Create your models here.
+class Room(Model):
+    name = models.CharField(max_length=200)
+    # s = None -> null
+    # s = "" -> blank
+    description = models.TextField(null=True, blank=True)
+    # vazba M:N pro uzivatele a mistnost
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    # auto_now_add = True -> Pouze pri vytvoreni zaznamu do databaze
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now = True -> Pri jakekoliv editaci zaznamu
+    updated = models.DateTimeField(auto_now=True)
+
+    # rika jak se ma vytisknout trida model
+    def __str__(self):
+         return self.name
+
+    class Meta:
+        # razeni podle created a pokud jsou stejne bude se radit podle nazvu
+        # a kdyz budou dve stejneho jmena a stejneho data vytvoreni seradit podle data a casu update
+        # ordering = ['-created', '-name', '-uppdate'] -> razeni DESC
+        ordering = ['created', 'name', 'updated']
+
+
+
+class Message(Model):
+    body = models.TextField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+    class Meta:
+        ordering = ['-created', '-updated']
+
--- base/views.py
+++ base/views.py
@@ -1,7 +1,25 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from base.models import Room
+
 
 # Create your views here.
 def hello(request):
     s = request.GET.get('s', '')
     return HttpResponse(f'Hello, {s} world!')
+
+def search(request):
+    q = request.GET.get('q', '')
+    if q == '':
+        return HttpResponse("Prosím zadejte hledaný výraz.")
+
+    # vyhledava jak v popisku tak v description
+    rooms = Room.objects.filter(
+        Q(description__contains=q) |
+        Q(name__contains=q)
+    )
+
+    context = {'q': q, 'rooms': rooms}
+    return render(request, 'base/search.html', context)
